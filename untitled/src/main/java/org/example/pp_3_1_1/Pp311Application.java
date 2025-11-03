340d557638f291a752af828d993779cfe9a6310b
Vol4ara <129746981+Volcharaitsme@users.noreply.github.com>
2025-11-03 22:33:49 +0300

    Deleting_HiddenHttpMethodFilter

--- untitled/src/main/java/org/example/pp_3_1_1/Pp311Application.java
+++ untitled/src/main/java/org/example/pp_3_1_1/Pp311Application.java
@@ -2,8 +2,6 @@ package org.example.pp_3_1_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class Pp311Application {
@@ -11,10 +9,4 @@ public class Pp311Application {
     public static void main(String[] args) {
         SpringApplication.run(Pp311Application.class, args);
     }
-
-    @Bean
-    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
-        return new HiddenHttpMethodFilter();
-    }
-
 }
