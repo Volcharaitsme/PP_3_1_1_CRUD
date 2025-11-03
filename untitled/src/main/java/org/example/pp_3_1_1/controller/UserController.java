836d1e306c75abc42f939649442e73d538daecab
Vol4ara <129746981+Volcharaitsme@users.noreply.github.com>
2025-11-03 22:54:16 +0300

    Update imports

--- untitled/src/main/java/org/example/pp_3_1_1/controller/UserController.java
+++ untitled/src/main/java/org/example/pp_3_1_1/controller/UserController.java
@@ -6,7 +6,12 @@ import org.example.pp_3_1_1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping
