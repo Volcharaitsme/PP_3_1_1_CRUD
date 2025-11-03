a5493449708cac80dc48d5be74952324715a6c8a
Vol4ara <129746981+Volcharaitsme@users.noreply.github.com>
2025-11-03 22:52:48 +0300

    Update imports

--- untitled/src/main/java/org/example/pp_3_1_1/model/User.java
+++ untitled/src/main/java/org/example/pp_3_1_1/model/User.java
@@ -1,21 +1,25 @@
 package org.example.pp_3_1_1.model;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column
+    @Column(name = "firtst_name")
     private String firstName;
 
-    @Column
+    @Column(name = "last_name")
     private String lastName;
 
     public User() {
